27eb3c4d3d875f8efabdab14fdd5a4850cbbd8a5
hadi gh <hadi77.hg@gmail.com>
2021-08-13 22:10:28 +0430

    created helpers

---
+++ Helpers/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+function site_url($uri){
+    return $_ENV['base_url'] . $uri ;
+}
+
+function assets_url($uri){
+    return site_url('assets/'.$uri)  ;
+}
+
+function random_element($arr){ 
+    shuffle($arr);
+    return array_pop($arr);
+}
+
+
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
-include "vendor/autoload.php";
-
+include "Bootstrap/init.php";
 // echo $_SERVER['REQUEST_URI'];
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-echo $_ENV['DB_NAME'];
+
+// echo site_url("post/21");
+
+//  <link rel="stylesheet" href="<?= assets_url('css/style.css')"> 
+
+echo random_element(['1,34,56,47,567,fgh,678gh,fghrdg,sdgdfg,dfg,fdg','asdasdasdasdasd',1,2,34,25,345,4565,7,567,5675678]);
+
